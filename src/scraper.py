a30fe242c0e83209e78c6261848dbb538a1fc4b0
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2023-04-01 16:22:24 +0200

    jurigged attempt 1

--- src/scraper.py
+++ src/scraper.py
@@ -6,10 +6,10 @@ import requests
 class Scraper:
 
     def get_data(self) -> List[Bond]:
-        print("Started get data")
+        print("Started get data d")
         headers = {'Accept-Encoding': 'identity'}
         response = requests.get("https://www.borsaitaliana.it/borsa/obbligazioni/mot/obbligazioni-euro/lista.html", headers=headers)
-        print("Ho preso" + response.text)
+        #print("Ho preso" + response.text)
         soup = BeautifulSoup(response.text, 'html.parser')
         
         print("scraper")
