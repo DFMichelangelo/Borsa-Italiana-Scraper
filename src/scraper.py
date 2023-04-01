02fa7bbc94cad52569a9b93cc27c20e629ac75e0
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2023-04-01 17:52:36 +0200

    WIP

--- src/scraper.py
+++ src/scraper.py
@@ -1,16 +1,44 @@
 from .bond import Bond
 from bs4 import BeautifulSoup
 from typing import List
+import urllib
 import requests
 
 class Scraper:
 
-    def get_data(self) -> List[Bond]:
-        print("Started get data d")
+    def analyze_single_bond(self, url):
+        print("Start analysing " + url)
         headers = {'Accept-Encoding': 'identity'}
-        response = requests.get("https://www.borsaitaliana.it/borsa/obbligazioni/mot/obbligazioni-euro/lista.html", headers=headers)
-        print("Ho preso" + response.text)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html5lib')
+        print("End")
+
+    def get_data_single_url(self, url) -> List[Bond]:
+        print("Started get data")
+        headers = {'Accept-Encoding': 'identity'}
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html5lib')
+        rows = soup.find("table").find("tbody").find_all("tr")
+        bonds = []
+        for row in rows:
+            single_row = row.find_all("td")
+            if single_row == None or len(single_row) == 0 : continue
+            href = single_row[0].find("a", href=True)['href']
+            #bond = self.analyze_single_bond(urllib.urlparse(url).urljoin(href))
+            bonds.append(bond)
+            print("-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-")
         
-        print("scraper")
-        return [Bond(),Bond(),Bond()]
+        print("End")
+        return bonds
+    
+    def get_data(self) -> List[Bond]:
+        urls = [
+            "https://www.borsaitaliana.it/borsa/obbligazioni/mot/obbligazioni-euro/lista.html",
+            "https://www.borsaitaliana.it/borsa/obbligazioni/extramot/lista.html"
+        ]
+        bonds = []
+        for url in urls:
+            single_bond_list = self.get_data_single_url(url)
+            bonds.append(single_bond_list)
+        
+        return bonds;
