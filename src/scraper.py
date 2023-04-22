d75357d19ef287883e2e28361f5967a22e2c8977
DFMichelangelo <df.michelangelo@gmail.com>
2023-04-22 21:44:12 +0200

    changed driver options

--- src/scraper.py
+++ src/scraper.py
@@ -24,9 +24,15 @@ class Scraper:
     self.amount = amount
     chrome_options = Options()
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('disable-infobars')
+    chrome_options.add_argument("--disable-extensions")
+    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path='/usr/bin/chromedriver')
     # chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
+    # driver = webdriver.Chrome(options=chrome_options)
     self.driver = driver
 
   def find_n_value_from_label(
