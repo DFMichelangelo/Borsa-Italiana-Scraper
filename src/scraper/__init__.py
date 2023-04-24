a0f1798bb57f0846e746da5ddcf98a263443442f
DFMichelangelo <df.michelangelo@gmail.com>
2023-04-24 23:53:26 +0200

    added headless

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -24,7 +24,7 @@ class Scraper:
     self.amount = 0
     chrome_options = Options()
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-dev-shm-usage')
